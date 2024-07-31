5e0cd651e44482191b69d43a9aaf2f20b3154d4a
Austinmc41 <austinmc41@gmail.com>
2024-07-30 23:13:01 -0700

    memory_cards.ai first modelds and boiler endpoint

--- app/routers/decks.py
+++ app/routers/decks.py
@@ -1,8 +1,30 @@
+from typing import List
+
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter()
+
+class Card(BaseModel):
+    front: str
+    back: str
+
+class Deck(BaseModel):
+    name: str
+    Cards: List[Card]
 
 
 @router.get("/decks/", tags=["decks"])
 async def read_decks():
     return [{"deck_name": "Algo"}, {"deck_name": "Python"}]
+
+
+@router.post("/deck/")
+def create_deck():
+    return {"message": "deck created"}
+
+
+# need to define objects and models based on mock data for deck
+
+
+
