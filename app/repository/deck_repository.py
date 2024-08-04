cc54f767b07f595f3538467c7bd75bd8ec1464ac
Austinmc41 <austinmc41@gmail.com>
2024-08-04 01:27:14 -0700

    create

--- app/repository/deck_repository.py
+++ app/repository/deck_repository.py
@@ -2,14 +2,22 @@ from sqlalchemy.orm import Session
 from typing import List, Optional, Type
 from pydantic import UUID4
 
+from app.models.deck import Deck
+from app.schemas.deck_schema import Deck as DeckIO
+
 
 class DeckRepository:
     def __init__(self, session: Session):
         self.session = session
 
-    def create(self):
-        pass
-    def get_all(self):
+    def create(self, data: DeckIO) -> Deck:
+        deck = Deck(**data.model_dump(exclude_none=True))
+        self.session.add(deck)
+        self.session.commit()
+        self.session.refresh(deck)
+        return DeckIO(id=deck.title, name=deck.cards)
+
+    def get_all(self) -> List[Optional[DeckIO]]:
         pass
     def get_deck(self):
         pass
